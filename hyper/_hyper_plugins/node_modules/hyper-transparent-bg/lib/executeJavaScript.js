d5983862f61827b66132c5050e769df91d2e5d08
foo <foo@gmail.com>
2022-09-03 01:40:33 -0400

    initial debian release

--- hyper/_hyper_plugins/node_modules/hyper-transparent-bg/lib/executeJavaScript.js
+++
@@ -1,5 +0,0 @@
-module.exports = (instance, command, callback) => {
-  const js = `(${command})(${callback ? callback : ''})`;
-  if (process.env.DEBUG) console.log(js);
-  instance.webContents.executeJavaScript(js);
-};
