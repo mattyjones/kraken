a7e17159fa49acd3310f0a6642908a2258472568
foo <foo@gmail.com>
2022-09-05 23:27:08 -0400

    add sudo and fix command for installing packages

--- scripts/lib/system.sh
+++ scripts/lib/system.sh
@@ -12,7 +12,7 @@ source util.sh
 # system_upgrade will update the package lists and any packages that are already installed to
 # the latest versions. This update is based on the source branch set in set_debian_source()
 system_upgrade() {
-  if [ ! "$(sudo apt update && apt upgrade)" ]; then
+  if [ ! "$(sudo apt-get update && sudo apt-get upgrade)" ]; then
     echo -e "\n\e[$red System upgrade failed\e[$default"
     echo ""
     return 1
@@ -31,7 +31,8 @@ set_debian_source() {
 
   local new_branch="$1"
   # shellcheck disable=2155
-  local current_branch=$(➜ ~ cat /etc/apt/sources.list | grep -m 1 "deb http://" | awk '{ print $3 }')
+  # FIXME SC2002
+  local current_branch=$(cat /etc/apt/sources.list | grep -m 1 "deb http://" | awk '{ print $3 }')
 
   if [ "$current_branch" == "$new_branch" ]; then
     echo "Release branch is already set to $new_branch"
